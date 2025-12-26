7a3bd400f5114029e659c8a93e1871a200013c68
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2025-12-26 14:55:43 +0530

    refactor(clients): remove unused ERC20_ADDRESS and JsonRpcProvider imports

--- src/core/clients.ts
+++ src/core/clients.ts
@@ -5,17 +5,9 @@ import { OrderManager } from "@/services/OrderManager";
 import { TransferEventsQueue } from "@/queues/TransferEventsQueue";
 import { OrderDatabase } from "@/database/handlers/OrderDatabase";
 import { OrdersController } from "@/controllers/OrdersController";
-
-import { JsonRpcProvider } from "ethers";
 import { OrderEventQueue } from "@/queues/OrderEventQueue";
 import { RedisOrderService } from "@/services/RedisOrderService";
 
-const ERC20_ADDRESS = "0xD98aFa5e340816A637Bd886D16E82F9C2106bB21";
-const provider = new JsonRpcProvider(
-  "https://data-seed-prebsc-1-s1.binance.org:8545"
-);
-
-/// core clients ///
 /// services ///
 export const orderService = new OrderDatabase();
 export const redisOrderService = new RedisOrderService(redisConnection);
