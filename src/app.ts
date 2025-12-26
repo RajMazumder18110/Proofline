f535a7dc48837c3b35f0f79128498ed77f52f98a
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2025-12-26 11:05:30 +0530

    feat(app): update server port configuration to use dynamic port from configs

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@
 import app from "@/core/express";
 import { erc20 } from "@/core/clients";
 import { logger } from "@/configs/logger";
+import { configs } from "@/configs";
 
 /// Start core clients ///
 await erc20.start();
 
-app.listen(3000, () => {
-  logger.info("Server is running on port 3000");
+app.listen(configs.application.server.port, () => {
+  logger.info(`Server is running on port ${configs.application.server.port}`);
 });
